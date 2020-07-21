77e85e8c456ddc41e83cbcdb487e4c8560624c39
Francesco Sessa <francesco.sessa@eclettica.net>
2020-07-21 15:45:39 +0200

    m

--- src/android/AugmentedImageActivitySceneform.java
+++ src/android/AugmentedImageActivitySceneform.java
@@ -15,14 +15,30 @@ package com.gj.arcoredraw;
  * limitations under the License.
  */
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.ux.ArFragment;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -46,6 +62,16 @@ public class AugmentedImageActivitySceneform extends AppCompatActivity {
   // Augmented image and its associated center pose anchor, keyed by the augmented image in
   // the database.
   private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
+  private Session session;
+  private boolean installRequested;
+  private boolean shouldConfigureSession = false;
+  private final boolean useSingleImage = false;
+
+
+
+  private static final String TAG = "ARPlugin: " + AugmentedImageActivitySceneform.class.getSimpleName();
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +86,140 @@ public class AugmentedImageActivitySceneform extends AppCompatActivity {
 
 
     arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+
+    installRequested = false;
   }
 
-  @Override
+  /*@Override
   protected void onResume() {
     super.onResume();
     if (augmentedImageMap.isEmpty()) {
       fitToScanView.setVisibility(View.VISIBLE);
     }
+  }*/
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (session == null) {
+      Exception exception = null;
+      String message = null;
+      try {
+        switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+          case INSTALL_REQUESTED:
+            installRequested = true;
+            return;
+          case INSTALLED:
+            break;
+        }
+
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+          CameraPermissionHelper.requestCameraPermission(this);
+          return;
+        }
+
+        session = new Session(/* context = */ this);
+      } catch (UnavailableArcoreNotInstalledException
+              | UnavailableUserDeclinedInstallationException e) {
+        message = "Please install ARCore";
+        exception = e;
+      } catch (UnavailableApkTooOldException e) {
+        message = "Please update ARCore";
+        exception = e;
+      } catch (UnavailableSdkTooOldException e) {
+        message = "Please update this app";
+        exception = e;
+      } catch (Exception e) {
+        message = "This device does not support AR";
+        exception = e;
+      }
+
+      if (message != null) {
+        SnackbarHelper.getInstance().showError(this, message);
+        Log.e(TAG, "Exception creating session", exception);
+        return;
+      }
+
+      shouldConfigureSession = true;
+    }
+
+    if (shouldConfigureSession) {
+      configureSession();
+      shouldConfigureSession = false;
+    }
+
+    // Note that order matters - see the note in onPause(), the reverse applies here.
+    try {
+      session.resume();
+    } catch (CameraNotAvailableException e) {
+      SnackbarHelper.getInstance().showError(this, "Camera not available. Try restarting the app.");
+      session = null;
+      return;
+    }
+    /*surfaceView.onResume();
+    displayRotationHelper.onResume();*/
+
+    if (augmentedImageMap.isEmpty()) {
+      fitToScanView.setVisibility(View.VISIBLE);
+    }
   }
+
+  private void configureSession() {
+    Config config = new Config(session);
+    config.setFocusMode(Config.FocusMode.AUTO);
+    if (!setupAugmentedImageDatabase(config)) {
+      SnackbarHelper.getInstance().showError(this, "Could not setup augmented image database");
+    }
+    session.configure(config);
+  }
+  private boolean setupAugmentedImageDatabase(Config config) {
+    AugmentedImageDatabase augmentedImageDatabase;
+
+    // There are two ways to configure an AugmentedImageDatabase:
+    // 1. Add Bitmap to DB directly
+    // 2. Load a pre-built AugmentedImageDatabase
+    // Option 2) has
+    // * shorter setup time
+    // * doesn't require images to be packaged in apk.
+    if (useSingleImage) {
+      Bitmap augmentedImageBitmap = loadAugmentedImageBitmap();
+      if (augmentedImageBitmap == null) {
+        return false;
+      }
+
+      augmentedImageDatabase = new AugmentedImageDatabase(session);
+      augmentedImageDatabase.addImage("image_name", augmentedImageBitmap);
+      // If the physical size of the image is known, you can instead use:
+      //     augmentedImageDatabase.addImage("image_name", augmentedImageBitmap, widthInMeters);
+      // This will improve the initial detection speed. ARCore will still actively estimate the
+      // physical size of the image as it is viewed from multiple viewpoints.
+    } else {
+      // This is an alternative way to initialize an AugmentedImageDatabase instance,
+      // load a pre-existing augmented image database.
+      try (InputStream is = getAssets().open("sample_database.imgdb")) {
+        augmentedImageDatabase = AugmentedImageDatabase.deserialize(session, is);
+      } catch (IOException e) {
+        Log.e(TAG, "IO exception loading augmented image database.", e);
+        return false;
+      }
+    }
+
+    config.setAugmentedImageDatabase(augmentedImageDatabase);
+    return true;
+  }
+
+  private Bitmap loadAugmentedImageBitmap() {
+    try (InputStream is = getAssets().open("default.jpg")) {
+      return BitmapFactory.decodeStream(is);
+    } catch (IOException e) {
+      Log.e(TAG, "IO exception loading augmented image bitmap.", e);
+    }
+    return null;
+  }
+
 
   /**
    * Registered with the Sceneform Scene object, this method is called at the start of each frame.
